f8f4b1ab69b50c855cae10f300a9da43e5ed90c3
eipporko <me@davidantunez.es>
2016-06-12 12:51:12 +0200

    Multiplexed output

--- software/clock.c
+++ software/clock.c
@@ -2,17 +2,23 @@
 #include <time.h>
 #include <wiringPi.h>
 
-void nixiePin(int p, int v){
-  if (p != -1) {
-    digitalWrite(p, v);
+void nixiePin(int pin, int value){
+  if (pin != -1) {
+    digitalWrite(pin, value);
   }
 }
 
-void nixiePins(int p1, int p2, int p4, int p8, int v){
-  nixiePin(p1,v&1);
-  nixiePin(p2,v&2);
-  nixiePin(p4,v&4);
-  nixiePin(p8,v&8);
+void nixiePins(int value, int address){
+  nixiePin(2, address&1);
+  nixiePin(1, address&2);
+  nixiePin(0, address&4);
+  
+  nixiePin(6, value&1);
+  nixiePin(5, value&2);
+  nixiePin(4, value&4);
+  nixiePin(3, value&8);
+
+  delay (12); //~=85 Hz
 }
 
 void initWiringPi() {
@@ -27,17 +33,27 @@ void initWiringPi() {
 int main ()
 {
   time_t now;
+  clock_t start, stop;
+
   struct tm * ntm;
 
   initWiringPi();
 
   while(1) {
+    start = clock() / (CLOCKS_PER_SEC / 1000);
     time (&now);
     ntm = localtime (&now);
     printf ("%d : %d : %d \n", ntm->tm_hour, ntm->tm_min, ntm->tm_sec);
-    nixiePins(6, 5, 4, 3, ntm->tm_sec%10);
 
-    delay(1000);
+    nixiePins(ntm->tm_hour/10, 0);
+    nixiePins(ntm->tm_hour%10, 1);
+    nixiePins(ntm->tm_min/10, 2);
+    nixiePins(ntm->tm_min%10, 3);
+    nixiePins(ntm->tm_sec/10, 4);
+    nixiePins(ntm->tm_sec%10, 5);
+
+    stop = clock() / (CLOCKS_PER_SEC / 1000);
+    delay(1000 - (stop-start));
   }
 
   return 0;
